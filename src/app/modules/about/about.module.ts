bcf785faf5d04734078801f24b4f309ea5bd66a8
Roman UNTILOV <runtilov@pentalog.com>
2018-07-24 17:51:43 +0300

    Add styles and bootstrap

--- src/app/modules/about/about.module.ts
+++ src/app/modules/about/about.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutPageComponent } from './containers/about-page/about-page.component';
 import {aboutRoutes} from './about.routes';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   imports: [
     CommonModule,
+    BsDropdownModule.forRoot(),
     aboutRoutes
   ],
   exports: [
