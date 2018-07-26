5864528c5410a4d5e834333166d6766e4603546c
Maria Turuta <mariaturuta@gmail.com>
2018-07-26 17:43:39 +0300

    finished todo pages

--- src/app/modules/todo/containers/todo/todo.component.ts
+++ src/app/modules/todo/containers/todo/todo.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import {TodoService} from '../../todo.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent implements OnInit, OnDestroy {
+export class TodoComponent implements OnInit{
 
-  todoss = [];
   contactForm: FormGroup;
   submitted = false;
+  public result = [];
 
   constructor(private todoService: TodoService, private formBuilder: FormBuilder) {
   }
@@ -18,33 +19,46 @@ export class TodoComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.contactForm = this.formBuilder.group({
       name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      site: ['', UrlValidator],
-      message: ['', Validators.required]
+      status: ['', Validators.required],
+      date: new Date(),
+      description: ['', Validators.required]
     });
-
-    this.todoss = this.todoService.getAllTodos();
+     this.onFindTodo('');
   }
 
-
-  ngOnDestroy() {
-  }
-
-
-  // convenience getter for easy access to form fields
   get f() {
     return this.contactForm.controls;
   }
-  onSubmit() {
-    this.submitted = true;
 
-    // stop here if form is invalid
-    if (this.contactForm.invalid) {
-      return;
-    }
+  onSubmit() {
+    if (this.contactForm.invalid) {return;}
+
+    this.submitted = true;
+    const data = this.contactForm.value;
+
+    this.todoService.addTodo(data);
+    const todos = this.todoService.getAllTodos();
+    console.log(todos);
 
     alert('SUCCESS!! 💪 \n\n' + JSON.stringify(this.contactForm.value));
   }
 
+  onFindTodo(name) {
+    //if (!name || name==='') {name='aa'}
+    this.result = this.todoService.getTodosByName(name);
+    console.log(JSON.stringify(this.result));
+    return this.result;
+  }
+
+  onDeleteOne(name) {
+    const todos = this.todoService.deleteTodo(name);
+    console.log(todos);
+  }
+
+  onDeleteAll() {
+    this.todoService.deleteAllTodos();
+    const todos = this.todoService.getAllTodos();
+    console.log(todos);;
+  }
 
 }
--- src/app/modules/todo/todo.service.ts
+++ src/app/modules/todo/todo.service.ts
@@ -1,13 +1,41 @@
 import { Injectable } from '@angular/core';
+import {FormBuilder} from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  private todoStorageKey = 'todos';
 
-  constructor() { }
-
-  getAllTodos(){
-    return [];
+  constructor(private todoService: TodoService) {
   }
+
+  addTodo(todoContent){
+    const todos = JSON.parse(localStorage.getItem(this.todoStorageKey)) || [];
+    todos.push(todoContent);
+    localStorage.setItem(this.todoStorageKey, JSON.stringify(todos));
+  }
+
+  getAllTodos() {
+    const todos = JSON.parse(localStorage.getItem(this.todoStorageKey)) || [];
+    return todos;
+  }
+
+  deleteTodo(name: string){
+    const todos = JSON.parse(localStorage.getItem(this.todoStorageKey)) || [];
+    const resultedTodos = todos.filter(todo => todo.name !== name);
+    localStorage.setItem(this.todoStorageKey, JSON.stringify(resultedTodos));
+    return resultedTodos;
+  }
+
+  deleteAllTodos(){
+    localStorage.removeItem(this.todoStorageKey);
+  }
+
+  getTodosByName(name: string) {
+    const todos = JSON.parse(localStorage.getItem(this.todoStorageKey)) || [];
+    const filteredTodos = todos.filter(todo => todo.name.includes(name));
+    return filteredTodos ;
+  }
+
 }
